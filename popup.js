8f86c2b4d644addfb95c14df7cf54b89f8b31060
Dan Brinkman <danbrink91@gmail.com>
2014-10-24 21:26:27 -0400

    moved all code into a background page, popupjs.

--- popup.js
+++ popup.js
@@ -1,42 +1,55 @@
-// auth section
-var authDiv = document.getElementById("auth");
-var usernameField = document.getElementById("username");
-var passwordField = document.getElementById("password");
-var loginButton = document.getElementById("login");
-
-var searchDiv = document.getElementById("search");
-var searchButton = document.getElementById("searchButton");
-var searchResults = document.getElementById("searchResults");
-
-var optionsDiv = document.getElementById("addOptions");
-
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
-	usernameField.value = msg.response;
-	if(msg.response){
-		authDiv.style.display = "none";
-		searchDiv.style.display = "block";
-	}
-
+var username;
+var password;
+chrome.storage.sync.get({
+  username: '',
+  password: ''
+}, function(items) {
+   	username = items.username;
+	password = items.password;
 });
+function addAnime(info, tab) {	
+	var oReq = new XMLHttpRequest();
+			
+	oReq.onload = function(){
+		var domParser = new DOMParser();
+		var dom = domParser.parseFromString(this.responseText, 'text/xml');
+		var animes = [];
+		for(var i = 0; i < dom.firstChild.childNodes.length; i++) {
+			var entry = dom.firstChild.childNodes[i];
+			if(entry.tagName=='entry') {
+				var anime = {};
+				for(var j = 0; j < entry.childNodes.length; j++){
+					var child = entry.childNodes[j];
+					switch(child.tagName){
+						case "image":
+							anime.image = child.innerHTML;
+							break;
+						case "title":
+							anime.title = child.innerHTML;
+						break;
+						case "id":
+							anime.id = child.innerHTML;
+						break;
+					}
+				}
+				animes.push(anime);
+			}
+		}
+		// for now just add the first search result to your list
+		var req = new XMLHttpRequest();
+		req.onload = function() {
+			console.log(this.responseText);
+		};
+		req.open("get", "http://myanimelist.net/api/animelist/add/"+animes[0].id+".xml", true, username, password);
+		req.send();			
+	};
 
-loginButton.addEventListener("click", function loginUser(e){
-		chrome.tabs.query({active:true, currentWindow:true}, function(tabs){		
-			chrome.tabs.sendMessage(tabs[0].id, {"user": usernameField.value, "password":passwordField.value},
-			 function(response){
-			 	// do nothing
-			});
-		});
+	oReq.open("get", "http://myanimelist.net/api/anime/search.xml?q="+encodeURIComponent(info['selectionText']), true, username, password);
+	oReq.send();
+}
+
+chrome.contextMenus.create({
+	"title": "Add Anime to MAL",
+	"contexts": ["selection"],
+	"onclick": addAnime
 });
-
-// searchButton.addEventListener("click", function searchAnime(e){
-	// chrome.tabs.query({active:true, currentWindow:true}, function(tabs){		
-	// 	chrome.tabs.sendMessage(tabs[0].id, {"search":true}, function(response){
-	// 		if(response.success){
-	// 			for(var i = 0, length = response.results.length; i < length; i++){
-	// 				searchResults.innerHTML += "<a href='' class='animeLink' data-id='"+response.results[i].id+
-	// 				"'>" + response.results[i].name + "</a>";
-	// 			}
-	// 		}
-	// 	});
-	// });
-// });
