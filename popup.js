e1d16093e9390d4f3c5274214969bb6230356de6
Dan Brinkman <DanBrink91@gmail.com>
2015-10-17 20:52:45 -0400

    fixed searching, still cannot add the anime

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-var username;
-var password;
+var username = '';
+var password = '';
 chrome.storage.sync.get({
   username: '',
   password: ''
@@ -7,49 +7,57 @@ chrome.storage.sync.get({
    	username = items.username;
 	password = items.password;
 });
-function addAnime(info, tab) {	
+
+function findAnime(info, tab) 
+{	
 	var oReq = new XMLHttpRequest();
 			
 	oReq.onload = function(){
+		var animes = [];
 		var domParser = new DOMParser();
 		var dom = domParser.parseFromString(this.responseText, 'text/xml');
-		var animes = [];
 		for(var i = 0; i < dom.firstChild.childNodes.length; i++) {
 			var entry = dom.firstChild.childNodes[i];
 			if(entry.tagName=='entry') {
 				var anime = {};
 				for(var j = 0; j < entry.childNodes.length; j++){
 					var child = entry.childNodes[j];
-					switch(child.tagName){
-						case "image":
-							anime.image = child.innerHTML;
-							break;
-						case "title":
-							anime.title = child.innerHTML;
-						break;
-						case "id":
-							anime.id = child.innerHTML;
-						break;
+					if(child.tagName)
+					{
+						anime[child.tagName] = child.innerHTML;
 					}
 				}
 				animes.push(anime);
 			}
 		}
+		console.log(animes);
 		// for now just add the first search result to your list
-		var req = new XMLHttpRequest();
-		req.onload = function() {
-			console.log(this.responseText);
-		};
-		req.open("get", "http://myanimelist.net/api/animelist/add/"+animes[0].id+".xml", true, username, password);
-		req.send();			
+		addAnime(animes[0]);		
 	};
 
+	oReq.onerror = function(e) {
+		console.log(e);
+	};	
 	oReq.open("get", "http://myanimelist.net/api/anime/search.xml?q="+encodeURIComponent(info['selectionText']), true, username, password);
 	oReq.send();
 }
 
+function addAnime(anime)
+{
+	var req = new XMLHttpRequest();
+	//req.withCredentials = true;
+	req.onload = function() {
+		console.log(this);
+	};
+	req.onerror = function(e){
+		console.log(e);
+	};
+	req.open("get", "http://myanimelist.net/api/animelist/add/"+anime.id+".xml", true, username, password);
+	var data = '<?xml version="1.0" encoding="UTF-8"?><entry><episode>1</episode><status>1</status><score>7</score><downloaded_episodes></downloaded_episodes><storage_type></storage_type><storage_value></storage_value><times_rewatched></times_rewatched><rewatch_value></rewatch_value><date_start></date_start><date_finish></date_finish><priority></priority><enable_discussion></enable_discussion><enable_rewatching></enable_rewatching><comments></comments><fansub_group></fansub_group><tags>test tag, 2nd tag</tags></entry>';
+	req.send("id="+anime.id+"&data=" + encodeURIComponent(data));
+}
 chrome.contextMenus.create({
 	"title": "Add Anime to MAL",
 	"contexts": ["selection"],
-	"onclick": addAnime
+	"onclick": findAnime
 });
